06d4b10050b53bdf997c926cfbc61945a0804bb8
PhilipeF <philipefdev@gmail.com>
2025-06-19 00:31:03 -0300

    Fortalecendo os conceitos

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import { useState } from "react";
+
 function App() {
-  return <>naked react - PhilipeF-DEV</>;
+  const [count, setCount] = useState(0)
+  const [showText, setShowText] = useState(true)
+
+  const increment = () => {
+    setCount(count + 1);
+    setShowText(!showText);
+  }
+
+  return (
+    <>
+      {count}
+      <button onClick={increment}>Clique aqui</button>
+      {showText && <p>Mostra texto</p>}
+    </>
+  )
 }
 
 export default App;
